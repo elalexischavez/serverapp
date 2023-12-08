b3c61f3fe606afde5186fc4170af8506e788e26d
elalexischavez <L201100105@iztapalapa3.tecnm.mx>
2023-12-08 11:32:31 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,44 @@
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
-const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-// Configurar CORS para permitir conexiones desde cualquier lugar en la intranet
-app.use(cors({
-  origin: '*',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-}));
+const cors = require("cors")
+
+app.use(cors());
+
+const clients = {}; // Almacenar información de los clientes conectados
 
 io.on("connection", (socket) => {
-    console.log("Nuevo cliente conectado\n ID asignado: " + socket.id);
+    console.log(`Nuevo cliente conectado. ID asignado: ${socket.id}`);
+
+    // Almacena la información del nuevo cliente
+    clients[socket.id] = { id: socket.id, location: null };
+
+    // Envía la información de todos los clientes a un cliente recién conectado
+    Object.keys(clients).forEach((clientId) => {
+        io.to(socket.id).emit("message", clients[clientId]);
+    });
+
+    // Escucha la ubicación del cliente
     socket.on("message", (msg) => {
-        console.log(msg);
-        io.emit("message", msg);
+        console.log(`Mensaje recibido de ${socket.id}:`, msg);
+
+        // Actualiza la ubicación del cliente en el servidor
+        clients[socket.id].location = msg;
+
+        // Emitir la información del cliente a todos los clientes
+        io.emit("message", clients[socket.id]);
+    });
+
+    // Manejar desconexiones de clientes
+    socket.on("disconnect", () => {
+        console.log(`Cliente desconectado. ID: ${socket.id}`);
+        // Elimina la información del cliente desconectado
+        delete clients[socket.id];
     });
 });
 
