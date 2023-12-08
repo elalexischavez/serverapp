58c3a4909e0139bd34bd6be8d405b16a73cfe5e5
elalexischavez <L201100105@iztapalapa3.tecnm.mx>
2023-12-08 00:47:00 -0600

    configure cors

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
+const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-const cors = require("cors")
-
-app.use(cors());
-
+// Configurar CORS para permitir conexiones desde cualquier lugar en la intranet
+app.use(cors({
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
 
 io.on("connection", (socket) => {
-    console.log("New client connected\n id asigned: " + socket.id);
+    console.log("Nuevo cliente conectado\n ID asignado: " + socket.id);
     socket.on("message", (msg) => {
         console.log(msg);
         io.emit("message", msg);
-    })
-})
+    });
+});
 
 const PORT = process.env.PORT || 80;
 server.listen(PORT, () => {
-    console.log('Servidor Socket.IO escuchando en el puerto ${PORT}');
+    console.log(`Servidor Socket.IO escuchando en el puerto ${PORT}`);
 });
